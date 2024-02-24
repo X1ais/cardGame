5ba18b49fae9e69f812da90e9a3ce7af6540439b
X1ais <chrissxjackson@gmail.com>
2024-02-23 23:20:49 -0700

    added background and card images

--- src/cardGame/App.java
+++ src/cardGame/App.java
@@ -5,18 +5,18 @@ import javax.swing.JFrame;
 public class App {
 
 	public static void main(String[] args) {
-
-//		Game exceed = new Game();
+		
+		GamePanel exceed = new GamePanel();
+		
+		exceed.newFrame();
+		exceed.startGame();
+//		GameFrame window = new GameFrame();
 //		
-//		exceed.openFrame();
-//		exceed.startGame();
-		GameFrame window = new GameFrame();
+//		GamePanel gamePanel = new GamePanel(200,200,0);
+//		window.add(gamePanel);
+//		window.pack();
 		
-		GamePanel gamePanel = new GamePanel(200,200,0);
-		window.add(gamePanel);
-		window.pack();
-		
-		gamePanel.startGameThread();
+//		gamePanel.startGameThread();
 
 	}
 
--- src/cardGame/GamePanel.java
+++ src/cardGame/GamePanel.java
@@ -1,46 +1,113 @@
 package cardGame;
 
 import java.awt.*;
+import java.awt.event.*;
+import java.io.IOException;
 
-import javax.swing.JPanel;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class GamePanel extends JPanel implements Runnable{
-	int xOrigin, yOrigin, radius = 100, ballRadius = 20;
-	double currentAngle;
-	Thread gameThread;
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6642625196859423117L;
+	private static int FPS = 30;
+	private static double drawTime = 1000000000/FPS;
 	
-	int FPS = 60;
-
-
-	public GamePanel(int xOrigin, int yOrigin, double initialAngle) {
-		this.xOrigin = xOrigin;
-		this.yOrigin = yOrigin;
-		this.currentAngle = initialAngle;
+	Thread thread;
+	
+	// Player
+	List<Card> playerHand = new ArrayList<>();
+	
+	int playerPosition;
+	// Opponent
+	List<Card> oppHand = new ArrayList<>();
+	int oppPosition; 
+	
+	// Window Dimensions
+	int windowWidth = 1920;
+	int windowHeight = 1080;
+	
+	// Card Dimensions
+	int cardWidth = 140;
+	int cardHeight = 196;
+	
+	JFrame frame = new JFrame("Exceed Fighting System");	
+	
+	public GamePanel() {
 		
-		this.setPreferredSize(new Dimension(400,400));
-		this.setBackground(Color.BLACK);
-		this.setDoubleBuffered(false);
-	}
-	
-	public void startGameThread() {
-		gameThread = new Thread(this);
-		gameThread.start();
 	}
 
+	public void newFrame() {
+		frame.setVisible(true);
+		frame.setSize(windowWidth, windowHeight);
+		frame.setLocationRelativeTo(null);
+		frame.setResizable(false);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		this.setLayout(new BorderLayout());
+		this.setBackground(new Color(27,27,27));
+		frame.add(this);
+	}
 	
+	public void startGame() {
+		Deck deck = new Deck();
+		
+		deck.shuffle();
+		
+		System.out.println("Starting Deck: ");
+		deck.describe();
+		
+		// Deal each player a starting hand.
+		deck.deal(playerHand, oppHand);
+	
+		System.out.println("Starting Hands:");
+		System.out.println("Player:");
+		System.out.println(playerHand);
+		System.out.println("Opponent:");
+		System.out.println(oppHand);
+		
+		// Set players' starting position
+		playerPosition = 2;
+		oppPosition = 6;
+		
+		//Print players' positions
+		System.out.println();
+		System.out.println("———————————————————————————");
+		for (int i = 0; i < 9; i++) {
+			System.out.print("[");
+			if (i == playerPosition) {
+				System.out.print("P");
+			} else if (i == oppPosition) {
+				System.out.print("O");
+			} else {
+				System.out.print(" ");
+			}
+			System.out.print("]");
+		}
+		System.out.println();
+		System.out.println("———————————————————————————");
+		
+		thread = new Thread(this);
+		thread.start();
+	}
+
 	@Override
 	public void run() {
-		
-		double drawInterval = 1000000000/FPS; // 16.66 milliseconds
-		double delta = 0;
 		long lastTime = System.nanoTime();
 		long currentTime;
+		double delta = 0;
 		
-		while(gameThread != null) { 	
+		while(thread != null) {
 			
+			// Display each frame 1/60 of a second (60FPS)
 			currentTime = System.nanoTime();
-			
-			delta += (currentTime - lastTime) / drawInterval;
+			delta += (currentTime - lastTime) / drawTime;
 			lastTime = currentTime;
 			
 			if (delta >= 1) {
@@ -48,31 +115,30 @@ public class GamePanel extends JPanel implements Runnable{
 				repaint();
 				delta--;
 			}
-			
 		}
 	}
 	
 	public void update() {
-		currentAngle += 2;
-	}
-
-	public void paintComponent(Graphics g) {
-		super.paintComponent(g);
-		// Draw Ball Path
-		g.setColor(Color.WHITE);
-		g.drawOval(xOrigin - radius, yOrigin - radius, radius * 2, radius * 2);
 		
-		
-		int ballOriginX, ballOriginY;
-		ballOriginX = (int) (radius * Math.cos(currentAngle * Math.PI / 180));
-		ballOriginY = (int) (radius * Math.sin(currentAngle * Math.PI / 180));
-		
-		// Draw Ball
-		g.setColor(Color.BLUE);
-		g.fillOval(xOrigin + ballOriginX - ballRadius, yOrigin + ballOriginY - ballRadius, ballRadius * 2, ballRadius * 2);
-		
-		g.dispose();
 	}
 	
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		try {
+//			Image backgroundImg = ImageIO.read(getClass().getResource("/background.png"));
+//			g.drawImage(backgroundImg,0,0,windowWidth,windowHeight,null);
+			
+			for (int i = 0; i < playerHand.size(); i++) {
+				Image cardImg = ImageIO.read(getClass().getResource("/blankCard.png"));
+				g.drawImage(cardImg,(windowWidth-(i+1)*cardWidth)/2,windowHeight-cardHeight/2,cardWidth, cardHeight,null);
+			}
+			
+			
+		} catch(IOException e) {
+			System.out.println(e.getMessage());
+		}
+		g.dispose();
+		
+	}
 	
 }
